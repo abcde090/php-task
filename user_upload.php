55d35a570a84156f47079777031568d107114445
abcde090 <229243386@qq.com>
2020-02-02 23:28:17 +1000

    finish create_table command

--- user_upload.php
+++ user_upload.php
@@ -8,6 +8,7 @@ $host = "127.0.0.1";
 $username = "root";
 $password = "";
 $dbname = "myDatabase";
+$conn = new mysqli($host, $username, $password);
 
 
 echo "Script starting now\n";
@@ -16,15 +17,14 @@ commandControl();
 
 function connection()
 {
-    global $host, $username, $password;
-    $conn = new mysqli($host, $username, $password);
+    global $conn;
     if ($conn->connect_error) {
         die('Error (' . $conn->connect_errno . ') ');
     } else echo "Successfully connected to MySQL server.\n";
 }
 
 function commandControl()
-{   
+{
     global $host, $username, $password;
     $shortOptions = "uph";
     $longOptions  = array(
@@ -41,6 +41,7 @@ function commandControl()
     }
 
     if (array_key_exists('create_table', $options)) {
+        table_creation();
     }
 
     if (array_key_exists('dry_run', $options) and array_key_exists('file', $options)) {
@@ -62,18 +63,45 @@ function commandControl()
     if (array_key_exists('help', $options)) {
         help_messages();
     }
-
-    else {
-        echo "Invalid command, enter --help for full commands list\n";
-    }
 }
 
-function help_messages() {
-    echo 
-    "    --file [csv file name] – this is the name of the CSV to be parsed\n 
+function help_messages()
+{
+    echo
+        "    --file [csv file name] – this is the name of the CSV to be parsed\n 
     --create_table – this will cause the MySQL users table to be built (and no further action will be taken)\n
     --dry_run – this will be used with the --file directive in the instance that we want to run the script but not insert into the DB. All other functions will be executed, but the database won't be altered.\n
     -u – MySQL username\n
     -p – MySQL password\n
     -h – MySQL host\n";
+}
+
+
+function table_creation()
+{
+    global $conn, $dbname;
+    $query = "CREATE DATABASE IF NOT EXISTS $dbname";
+    if (mysqli_query($conn, $query)) {
+        echo "Success creating database\n";
+    } else {
+        echo "Failure creating database\n";
+    }
+
+    $db_selected = mysqli_select_db($conn, $dbname);
+    if (!$db_selected) {
+        die('Cannot use the selected database');
+    }
+    $sql = "CREATE TABLE IF NOT EXISTS userTable (
+    name VARCHAR(30) NOT NULL,
+    surname VARCHAR(30) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    UNIQUE (email)
+    )";
+
+
+    if (mysqli_query($conn, $sql)) {
+        echo "Table userTable created successfully\n";
+    } else {
+        echo "Error: " . mysqli_error($conn) . "\n";
+    }
 }
